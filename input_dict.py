ddd81021a1015fa50170caed79915ad4fff7e01f
Ryan Loomis <rloomis@cvpost042.cv.nrao.edu>
2020-07-07 15:15:22 -0400

    initial commit

---
+++ input_dict.py
@@ -0,0 +1,9 @@
+input_dict = {}
+input_dict['mol_name'] = 'hc9n_hfs'
+input_dict['fit_folder'] = '/folder/to/hold/fit/results'
+input_dict['cat_folder'] = '/catalog/folder/path'
+input_dict['data_path'] = '/path/to/data/TMC1_GOTHAM.npy'
+input_dict['prior_path'] = '/path/to/prior/chain.npy'
+input_dict['block_interlopers'] = True
+input_dict['nruns'] = 4000
+input_dict['restart'] = True

c99a1bbcd10f2934b25e4b1bb4704916b4933006
laserkelvin <kelvlee@mit.edu>
2020-08-05 10:07:58 -0400

    Also pulling trimmed catalogs

--- pull_data.sh
+++ pull_data.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-rclone copy dropbox:Projects/GOTHAM/data/data ./raw/
+GOT_PATH=Projects/GOTHAM/data
+
+rclone sync dropbox:$GOT_PATH/data ./raw
+
+rclone sync dropbox:$GOT_PATH/phase1_SI/supplementary ./supplementary
+
+rclone sync dropbox:$GOT_PATH/gotham_catalogs_trimmed ./gotham_catalogs
+
